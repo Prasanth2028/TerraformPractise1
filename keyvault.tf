5f1c9dec723bd355277a01ecb1af9f1f337d13c2
Prasanth2028 <prasanth.k2828@gmail.com>
2025-10-13 19:14:53 +0530

    uopdating

--- keyvault.tf
+++ keyvault.tf
@@ -1,10 +1,8 @@
-# module "Keyvault" {
-#   source             = "git::https://github.com/ModuleASDA/Key-Vault.git"
-#   # key_vault_rg_name  = data.azurerm_resource_group.example.name
-#   # key_vault_location = data.azurerm_resource_group.example.location
-#   key_vault_rg_name  = azurerm_resource_group.main.name
-#   key_vault_location = azurerm_resource_group.main.location
-#   key_vault_name     = "kv-01-terraform-practise"
-#   key_vault_sku_name = "standard"
-# }
+module "Keyvault" {
+  source             = "git::https://github.com/ModuleASDA/Key-Vault.git"
+  key_vault_rg_name  = data.azurerm_resource_group.example.name
+  key_vault_location = data.azurerm_resource_group.example.location
+  key_vault_name     = "kv-01-terraform-practise"
+  key_vault_sku_name = "standard"
+}
 
