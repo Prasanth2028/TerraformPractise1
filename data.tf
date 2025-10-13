3327cc3bffefd18bfd0f7ff6b60af4a7b4693f84
Prasanth2028 <prasanth.k2828@gmail.com>
2025-10-13 18:53:57 +0530

    CheckingPlan

--- data.tf
+++ data.tf
@@ -1,3 +1,9 @@
 data "azurerm_resource_group" "example" {
   name = "Resource_Group_Terraform_03"
 }
+
+resource "azurerm_resource_group" "main" {
+  name     = testing001
+  location = "West Europe"
+  #tags     = var.tags # Uncomment and define var.tags in variables.tf if you want to use tags
+}
